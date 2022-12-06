eb878a2294db681a7c347d69ef86d63227479f24
Senne <senne.de.neef@student.ehb.be>
2022-12-06 14:54:15 +0100

    split layout met camera, nog geen scanner

--- app/src/main/java/be/senne/qr_demo/MainActivity.kt
+++ app/src/main/java/be/senne/qr_demo/MainActivity.kt
@@ -1,10 +1,9 @@
 package be.senne.qr_demo
 
-import android.Manifest.permission.CAMERA
-import android.content.Context
-import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.TypedValue
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.core.CameraSelector
@@ -12,9 +11,8 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -23,16 +21,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.scale
 import be.senne.qr_demo.ui.theme.Qr_demoTheme
-import com.google.common.util.concurrent.ListenableFuture
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import net.glxn.qrgen.android.QRCode
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.KeyPairGenerator
@@ -40,26 +40,21 @@ import java.security.SecureRandom
 import java.security.Security
 import java.security.spec.ECGenParameterSpec
 import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+
 
 class MainActivity : ComponentActivity() {
 
     companion object {
-        lateinit var context : Context
+        lateinit var resources : Resources
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        context = applicationContext
+        MainActivity.resources = resources
 
         Security.removeProvider("BC");
         Security.addProvider(BouncyCastleProvider())
 
         super.onCreate(savedInstanceState)
-
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0);
-        }
 
         setContent {
             Qr_demoTheme {
@@ -68,8 +63,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    createQrGenerator()
-                    createQrScanner()
+                    createQrScreen()
                 }
             }
         }
@@ -77,80 +71,96 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun createQrScanner() {
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(context)
+fun createQrScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(5f)
+                .padding(8.dp), contentAlignment = Alignment.Center) {
+            createQrGenerator()
+        }
+
+        Spacer(modifier = Modifier
+            .size(5.dp)
+            .weight(1f))
+
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .weight(5f)
+                .padding(8.dp)) {
+            createQrScanner()
+        }
     }
+}
+
+private fun generatePublicKeyQr() : Bitmap {
+    val ecParameter = ECGenParameterSpec("P-521")
+    val keygen = KeyPairGenerator.getInstance("ECDH", "BC");
+    keygen.initialize(ecParameter, SecureRandom())
+    val key = keygen.genKeyPair()
+
+    val keyBytes = key.public.encoded
+    val keyBase64 = Base64.getEncoder().encodeToString(keyBytes)
+
+    val qrCode = QRCode.from(keyBase64)
+
+    val px = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 380f, MainActivity.resources.displayMetrics))
 
 
-    AndroidView(factory = { viewContext ->
-        val previewView = PreviewView(viewContext)
-        val executor = ContextCompat.getMainExecutor(viewContext)
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder().build().also { preview ->
-                preview.setSurfaceProvider(previewView.surfaceProvider)
-            }
-            val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+    var qrBitmap = qrCode.withSize(px, px).bitmap()
+    println("${qrBitmap.width}")
+    println("${qrBitmap.height}")
 
-            cameraProvider.unbindAll()
-            cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
-
-        }, executor)
-        previewView
-    }, modifier = Modifier.fillMaxSize())
-    
-    val cameraSelector: CameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-    val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
-
-
-    cameraProviderFuture.addListener({
-        val cameraProvider = cameraProviderFuture.get()
-    }, ContextCompat.getMainExecutor(MainActivity.context))
-
+    return qrBitmap
 }
 
 @Composable
 fun createQrGenerator() {
+    val bitmap = remember { mutableStateOf(generatePublicKeyQr()) }
+    Image(bitmap = bitmap.value.asImageBitmap(), contentDescription = "public_key", Modifier.clickable(onClick = {
+        bitmap.value = generatePublicKeyQr()
+    }))
+}
 
-    val bitmap = remember { mutableStateOf(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)) }
 
-    LazyColumn(content = {
-        item {
-            Button(onClick = {
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun createQrScanner() {
+    val cameraPermissionState = rememberPermissionState(permission = android.Manifest.permission.CAMERA)
 
-            }) {
-                Text(text = "Scan")
+    if(!cameraPermissionState.status.isGranted) {
+        Column() {
+            Text("Ik heb camera permissie nodig om QR codes te scannen.")
+            Button(onClick = {cameraPermissionState.launchPermissionRequest()}) {
+                Text(text = "Geef Camera Permissie")
             }
         }
-        item {
-            Button(onClick = {
-                val ecParameter = ECGenParameterSpec("P-521")
-                val keygen = KeyPairGenerator.getInstance("ECDH", "BC");
-                keygen.initialize(ecParameter, SecureRandom())
-                val key = keygen.genKeyPair()
-
-                val keyBytes = key.public.encoded
-                val keyBase64 = Base64.getEncoder().encodeToString(keyBytes)
-
-                val qrCode = QRCode.from(keyBase64)
-                var qrBitmap = qrCode.bitmap()
-                qrBitmap = qrBitmap.scale(qrBitmap.width * 10, qrBitmap.height * 10)
-                bitmap.value = qrBitmap
-
-            }) {
-                Text(text = "Generate Qr code")
-            }
+    }
+    else {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val context = LocalContext.current
+        val cameraProviderFuture = remember {
+            ProcessCameraProvider.getInstance(context)
         }
+        AndroidView(factory = { viewContext ->
+            val previewView = PreviewView(viewContext)
+            val executor = ContextCompat.getMainExecutor(viewContext)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build().also { preview ->
+                    preview.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                val cameraSelector =
+                    CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
 
-        item {
-            Image(bitmap = bitmap.value.asImageBitmap(), contentDescription = "public_key")
-        }
-    })
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview)
 
-    Column(Modifier.fillMaxWidth()) {
-        
+            }, executor)
+            previewView
+        }, modifier = Modifier.fillMaxSize())
     }
 }
